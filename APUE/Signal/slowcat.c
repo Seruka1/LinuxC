c9b81e17f0a957754ba51f9f08323d2b125e03bc
Seruka1 <1106296366@qq.com>
2023-07-07 20:57:37 +0800

    deleted:    APUE/Signal/mycat.c 	new file:   APUE/Signal/slowcat.c 	new file:   APUE/Signal/slowcat2.c

---
+++ APUE/Signal/slowcat.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <signal.h>
+
+// 漏桶
+#define SIZE 10
+
+static int loop = 0;
+
+static void alrm_handler(int s)
+{
+    alarm(1);
+    loop = 1;
+}
+
+int main(int argc, char **argv)
+{
+    if (argc < 2)
+    {
+        fprintf(stdout, "Usage...");
+        exit(1);
+    }
+    int sfd = open(argv[1], O_RDONLY);
+    if (sfd < 0)
+    {
+        strerror(errno);
+        exit(1);
+    }
+    int dfd = 1;
+
+    signal(SIGALRM, alrm_handler);
+    alarm(1);
+
+    char buf[SIZE];
+
+    while (1)
+    {
+        while (loop == 0)
+        {
+            pause();
+        }
+        loop = 0;
+        int len = 0;
+        int pos = 0;
+        int ret = 0;
+        int fg = 0;
+        while ((len = read(sfd, buf, SIZE)) < 0)
+        {
+            if (errno == EINTR)
+            {
+                continue;
+            }
+            printf("%s\n", strerror(errno));
+            fg = 1;
+            break;
+        }
+        if (len == 0 || fg == 1)
+        {
+            break;
+        }
+        while (len > 0)
+        {
+            ret = write(dfd, buf + pos, len);
+            if (ret < 0)
+            {
+                printf("%s\n", strerror(errno));
+                // 注意这里直接退出会发生内存泄漏
+                exit(1);
+            }
+            pos += ret;
+            len -= ret;
+        }
+        // sleep在不同操作系统上的实现不一样
+        // sleep(1);
+    }
+
+    close(sfd);
+
+    exit(0);
+}
