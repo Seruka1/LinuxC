ab7c7453213f02e580421b15548fc7526306f736
Seruka1 <1106296366@qq.com>
2023-09-01 10:04:00 +0800

    modified:   WebServer/webserver_nk/a.out 	modified:   WebServer/webserver_nk/http_conn.cpp 	modified:   WebServer/webserver_nk/http_conn.h 	modified:   WebServer/webserver_nk/main.cpp 	new file:   WebServer/webserver_nk/resources/1B4185CE05F1852FFE990419B46D8A99.png:Zone.Identifier 	new file:   WebServer/webserver_nk/resources/images/image1.jpg 	new file:   WebServer/webserver_nk/resources/index.html 	new file:   "WebServer/webserver_nk/resources/\346\265\201\347\250\213\345\233\276.png"

--- WebServer/webserver_nk/http_conn.h
+++ WebServer/webserver_nk/http_conn.h
@@ -1,45 +1,59 @@
 #ifndef HTTPCONNECTION_H
 #define HTTPCONNECTION_H
 
-#include<sys/epoll.h>
-#include<stdio.h>
-#include<stdlib.h>
-#include<signal.h>
-#include<unistd.h>
-#include<sys/types.h>
-#include<fcntl.h>
-#include<sys/socket.h>
-#include<netinet/in.h>
-#include<arpa/inet.h>
-#include<sys/stat.h>
-#include<sys/mman.h>
-#include<stdarg.h>
-#include<errno.h>
-#include"locker.h"
-#include<sys/uio.h>
-#include<string.h>
-
+#include <sys/epoll.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <stdarg.h>
+#include <errno.h>
+#include "locker.h"
+#include <sys/uio.h>
+#include <string.h>
 
 class http_conn
 {
 public:
-    static int m_epollfd;   //所有socket上的事件都被注册到同一个epoll
-    static int m_user_count; //统计用户的数量
-    static const int READ_BUFFER_SIZE=2048;    //读缓冲区的大小
-    static const int WRITE_BUFFER_SIZE=2048;    //写缓冲区大小
-
+    static int m_epollfd;                      // 所有socket上的事件都被注册到同一个epoll
+    static int m_user_count;                   // 统计用户的数量
+    static const int READ_BUFFER_SIZE = 2048;  // 读缓冲区的大小
+    static const int WRITE_BUFFER_SIZE = 2048; // 写缓冲区大小
+    static const int FILENAME_LEN = 200;       // 文件名的最大长度
 public:
- // HTTP请求方法，这里只支持GET
-    enum METHOD {GET = 0, POST, HEAD, PUT, DELETE, TRACE, OPTIONS, CONNECT};
-    
+    // HTTP请求方法，这里只支持GET
+    enum METHOD
+    {
+        GET = 0,
+        POST,
+        HEAD,
+        PUT,
+        DELETE,
+        TRACE,
+        OPTIONS,
+        CONNECT
+    };
+
     /*
         解析客户端请求时，主状态机的状态
         CHECK_STATE_REQUESTLINE:当前正在解析请求行
         CHECK_STATE_HEADER:     当前正在解析头部字段
         CHECK_STATE_CONTENT:    当前正在解析请求体
     */
-    enum CHECK_STATE { CHECK_STATE_REQUESTLINE = 0, CHECK_STATE_HEADER, CHECK_STATE_CONTENT };
-    
+    enum CHECK_STATE
+    {
+        CHECK_STATE_REQUESTLINE = 0,
+        CHECK_STATE_HEADER,
+        CHECK_STATE_CONTENT
+    };
+
     /*
         服务器处理HTTP请求的可能结果，报文解析的结果
         NO_REQUEST          :   请求不完整，需要继续读取客户数据
@@ -51,54 +65,97 @@ public:
         INTERNAL_ERROR      :   表示服务器内部错误
         CLOSED_CONNECTION   :   表示客户端已经关闭连接了
     */
-    enum HTTP_CODE { NO_REQUEST, GET_REQUEST, BAD_REQUEST, NO_RESOURCE, FORBIDDEN_REQUEST, FILE_REQUEST, INTERNAL_ERROR, CLOSED_CONNECTION };
-    
+    enum HTTP_CODE
+    {
+        NO_REQUEST,
+        GET_REQUEST,
+        BAD_REQUEST,
+        NO_RESOURCE,
+        FORBIDDEN_REQUEST,
+        FILE_REQUEST,
+        INTERNAL_ERROR,
+        CLOSED_CONNECTION
+    };
+
     // 从状态机的三种可能状态，即行的读取状态，分别表示
     // 1.读取到一个完整的行 2.行出错 3.行数据尚且不完整
-    enum LINE_STATUS { LINE_OK = 0, LINE_BAD, LINE_OPEN };
-
-
+    enum LINE_STATUS
+    {
+        LINE_OK = 0,
+        LINE_BAD,
+        LINE_OPEN
+    };
 
 public:
-    http_conn(){}
-    ~http_conn(){}
+    http_conn();
+    ~http_conn();
 
-    void process(); //处理客户端请求
-    void init(int sockfd,const sockaddr_in &addr);
-    void close_conn();//关闭连接
-    bool read();      //非阻塞的读
-    bool write();     //非阻塞的写
-
+    void process(); // 处理客户端请求
+    void init(int sockfd, const sockaddr_in &addr);
+    void close_conn(); // 关闭连接
+    bool read();       // 非阻塞的读
+    bool write();      // 非阻塞的写
 
 private:
-    int m_sockfd;   //该HTTP连接的socket
-    sockaddr_in m_address; //通信的socket地址
-    char m_read_buf[READ_BUFFER_SIZE]; //读缓冲区
-    int m_read_idx; //标识读缓冲区中已经读入的客户端数据的最后一个字节的下一个位置
-    
-    int m_checked_index;  //当前正在分析的字符再读缓冲区的位置
-    int m_start_line;     //当前正在解析的行的起始位置
-    char* m_url;          //请求目标文件的文件名
-    char* m_version;      //协议版本，只支持HTTP1.1
-    METHOD m_method;      //请求方法
-    char* m_host;         //主机名
+    int m_sockfd;                      // 该HTTP连接的socket
+    sockaddr_in m_address;             // 通信的socket地址
+    char m_read_buf[READ_BUFFER_SIZE]; // 读缓冲区
+    int m_read_idx;                    // 标识读缓冲区中已经读入的客户端数据的最后一个字节的下一个位置
+
+    int m_checked_index; // 当前正在分析的字符再读缓冲区的位置
+    int m_start_line;    // 当前正在解析的行的起始位置
+
+    char *m_url;                    // 请求目标文件的文件名
+    char *m_version;                // 协议版本，只支持HTTP1.1
+    METHOD m_method;                // 请求方法
+    char *m_host;                   // 主机名
+    long m_content_len;             // HTTP请求体的消息总长度
     bool m_linger;                  // HTTP 请求是否要保持连接 keep-alive
+    char m_real_file[FILENAME_LEN]; // 客户请求的目标文件的完整路径，其内容等于 doc_root + m_url, doc_root是网站根目录
+    CHECK_STATE m_check_state;      // 主状态机当前所处的状态
 
+    struct stat m_file_stat;             // 目标文件的状态。通过它我们可以判断文件是否存在、是否为目录、是否可读，并获取文件大小等信息
+    char *m_file_address;                // 客户请求的目标文件被mmap到内存中的起始位置
+    char m_write_buf[WRITE_BUFFER_SIZE]; // 写缓冲区
+    int m_write_idx;                     // 写缓冲区中待发送的字节数
+    struct iovec m_iv[2];                // writev来执行写操作，表示分散写两个不连续内存块的内容
+    /*
+        struct iovec {
+        void *iov_base;   // 缓冲区的起始地址
+        size_t iov_len;   // 缓冲区的长度
+        };
+        iov_base：表示缓冲区的起始地址，通常是一个指向内存区域的指针。
+        iov_len：表示缓冲区的长度，即缓冲区中可用数据的字节数。
+        iovec 结构体通常用于 I/O 操作中，如在 readv()、writev() 等函数中，可以通过组合多个 iovec 结构体来一次性读取或写入多个缓冲区的数据，从而提高效率。
 
-    CHECK_STATE m_check_state; //主状态机当前所处的状态
+    */
+    int m_iv_count;      // 被写内存块的数量
+    int bytes_to_send;   // 将要发送的字节
+    int bytes_have_send; // 已经发送的字节
 
-    void init();                    // 私有函数，初始化连接以外的信息
-    HTTP_CODE process_read();                   //解析HTTP请求
-    HTTP_CODE parse_request_line(char* text);   //解析HTTP请求首行
-    HTTP_CODE parse_headers(char* text);        //解析请求头
-    HTTP_CODE parse_content(char* text);        //解析请求体
-    LINE_STATUS parse_line();                   //解析具体某一行  从状态机
-    char* get_lint(){return m_read_buf+m_start_line;}
+private:
+    void init();                       // 私有函数，初始化连接以外的信息
+    HTTP_CODE process_read();          // 解析HTTP请求
+    bool process_write(HTTP_CODE ret); // 填充HTTP应答
 
-    HTTP_CODE do_request();                         // 处理具体请求
+    // 下面这一组函数被process_read调用以分析HTTP请求
+    HTTP_CODE parse_request_line(char *text);              // 解析HTTP请求首行
+    HTTP_CODE parse_headers(char *text);                   // 解析请求头
+    HTTP_CODE parse_content(char *text);                   // 解析请求体
+    LINE_STATUS parse_line();                              // 解析具体某一行  从状态机
+    char *get_lint() { return m_read_buf + m_start_line; } // 获取一行数据
+    HTTP_CODE do_request();                                // 处理具体请求
 
+    // 这一组函数被process_write调用以填充HTTP应答。
+    void unmap();
+    bool add_response(const char *format, ...);
+    bool add_content(const char *content);
+    bool add_content_type();
+    bool add_status_line(int status, const char *title);
+    void add_headers(int content_length);
+    bool add_content_length(int content_length);
+    bool add_linger();
+    bool add_blank_line();
 };
-
-
 
 #endif
--- WebServer/webserver_nk/main.cpp
+++ WebServer/webserver_nk/main.cpp
@@ -5,7 +5,7 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <error.h>
-#include <fcntl.h>      
+#include <fcntl.h>
 #include <sys/epoll.h>
 #include <signal.h>
 #include <assert.h>
@@ -13,143 +13,141 @@
 #include "threadpool.h"
 #include "http_conn.h"
 
-#define MAX_FD 65535 //最大的文件描述符个数
-#define MAX_EVENT_NUMBER 10000 //最大监听事件数量
+#define MAX_FD 65535           // 最大的文件描述符个数
+#define MAX_EVENT_NUMBER 10000 // 最大监听事件数量
 
-//添加信号处理函数
-void addsig(int sig,void(handler)(int))
+// 添加信号处理函数
+void addsig(int sig, void(handler)(int))
 {
     struct sigaction sa;
-    memset(&sa,'\0',sizeof(sa));
-    sa.sa_handler=handler;
+    memset(&sa, '\0', sizeof(sa));
+    sa.sa_handler = handler;
     sigfillset(&sa.sa_mask);
-    sigaction(sig,&sa,NULL);
+    sigaction(sig, &sa, NULL);
 }
 
-//添加文件描述符到epoll中
-extern void addfd(int epollfd,int fd,bool one_shot);
+// 添加文件描述符到epoll中
+extern void addfd(int epollfd, int fd, bool one_shot);
 
-//删除epoll中的文件描述符
-extern void removefd(int epollfd,int fd);
+// 删除epoll中的文件描述符
+extern void removefd(int epollfd, int fd);
 
-//修改epoll中的文件描述符
-extern void modfd(int epollfd,int fd,int ev);
+// 修改epoll中的文件描述符
+extern void modfd(int epollfd, int fd, int ev);
 
-int main(int argc,char* argv[])
+int main(int argc, char *argv[])
 {
-    if(argc<=1)
+    if (argc <= 1)
     {
-        printf("按照如下格式运行：%s port_number\n",basename(argv[0]));
+        printf("按照如下格式运行：%s port_number\n", basename(argv[0]));
         exit(-1);
     }
 
-    //获取端口号
-    int port=atoi(argv[1]);
-    
-    //对sigpipe信号进行处理
-    addsig(SIGPIPE,SIG_IGN);
+    // 获取端口号
+    int port = atoi(argv[1]);
 
-    //创建线程池
-    threadpool<http_conn>* pool=NULL;
+    // 对sigpipe信号进行处理
+    addsig(SIGPIPE, SIG_IGN);
+
+    // 创建线程池
+    threadpool<http_conn> *pool = NULL;
     try
     {
-        pool=new threadpool<http_conn>;
-    }catch(...)
+        pool = new threadpool<http_conn>;
+    }
+    catch (...)
     {
         exit(-1);
     }
 
-    //创建一个数组用于保存所有的客户端信息
-    http_conn* users=new http_conn[MAX_FD];
+    // 创建一个数组用于保存所有的客户端信息
+    http_conn *users = new http_conn[MAX_FD];
 
-    int listenfd=socket(PF_INET,SOCK_STREAM,0);
-    
-    //设置端口复用
-    int reuse=1;
-    setsockopt(listenfd,SOL_SOCKET,SO_REUSEADDR,&reuse,sizeof(reuse));
+    int listenfd = socket(PF_INET, SOCK_STREAM, 0);
 
-    //绑定
+    // 设置端口复用
+    int reuse = 1;
+    setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse));
+
+    // 绑定
     struct sockaddr_in address;
-    address.sin_family=AF_INET;
-    address.sin_addr.s_addr=INADDR_ANY;
-    address.sin_port=htons(port);
-    bind(listenfd,(struct sockaddr*)&address,sizeof(address));
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(port);
+    bind(listenfd, (struct sockaddr *)&address, sizeof(address));
 
-    //监听
-    listen(listenfd,5);
+    // 监听
+    listen(listenfd, 5);
 
-    //创建epoll对象，事件数组，添加
+    // 创建epoll对象，事件数组，添加
     epoll_event events[MAX_EVENT_NUMBER];
-    int epollfd=epoll_create(5);
+    int epollfd = epoll_create(5);
 
-    //将监听的文件描述符添加到epoll对象中
-    addfd(epollfd,listenfd,false);
-    http_conn::m_epollfd=epollfd;
+    // 将监听的文件描述符添加到epoll对象中
+    addfd(epollfd, listenfd, false);
+    http_conn::m_epollfd = epollfd;
 
-    while(true)
+    while (true)
     {
-        int num=epoll_wait(epollfd,events,MAX_EVENT_NUMBER,-1);
-        if(num<0&&errno!=EINTR)
+        int num = epoll_wait(epollfd, events, MAX_EVENT_NUMBER, -1);
+        if (num < 0 && errno != EINTR)
         {
             printf("epoll failure\n");
             break;
         }
 
-        //循环遍历事件数组
-        for(int i=0;i<num;++i)
+        // 循环遍历事件数组
+        for (int i = 0; i < num; ++i)
         {
-            int sockfd=events[i].data.fd;
-            if(sockfd==listenfd)
+            int sockfd = events[i].data.fd;
+            if (sockfd == listenfd)
             {
-                //有客户端连接进来
+                // 有客户端连接进来
 
                 struct sockaddr_in client_address;
-                socklen_t client_addrlen=sizeof(client_address);
-                int connfd=accept(listenfd,(struct sockaddr*)&client_address,&client_addrlen);
+                socklen_t client_addrlen = sizeof(client_address);
+                int connfd = accept(listenfd, (struct sockaddr *)&client_address, &client_addrlen);
 
-                if(http_conn::m_user_count>=MAX_FD)
+                if (http_conn::m_user_count >= MAX_FD)
                 {
-                    //目前连接数满了
-                    //给客户端写个信息：服务器正忙。
+                    // 目前连接数满了
+                    // 给客户端写个信息：服务器正忙。
                     close(connfd);
                     continue;
                 }
                 // 将新客户的数据初始化放入数组当中
-                users[connfd].init(connfd,client_address);
+                users[connfd].init(connfd, client_address);
             }
-            else if(events[i].events&(EPOLLRDHUP|EPOLLHUP|EPOLLERR))
+            else if (events[i].events & (EPOLLRDHUP | EPOLLHUP | EPOLLERR))
             {
-                //客户端异常或者错误事件
+                // 客户端异常或者错误事件
                 users[sockfd].close_conn();
             }
-            else if(events[i].events& EPOLLIN)
+            else if (events[i].events & EPOLLIN)
             {
-                if(users[sockfd].read())
+                if (users[sockfd].read())
                 {
-                    //一次性把所有数据读完
-                    pool->append(users+sockfd);
+                    // 一次性把所有数据读完
+                    pool->append(users + sockfd);
                 }
                 else
                 {
                     users[sockfd].close_conn();
                 }
             }
-            else if(events[i].events&EPOLLOUT)
+            else if (events[i].events & EPOLLOUT)
             {
-                if(!users[sockfd].write())
+                if (!users[sockfd].write())
                 {
                     users[sockfd].close_conn();
                 }
             }
         }
-
-
     }
     close(epollfd);
     close(listenfd);
-    delete []users;
+    delete[] users;
     delete pool;
-
 
     return 0;
 }
